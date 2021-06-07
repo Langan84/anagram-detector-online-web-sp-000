2000ca19087f4187f49653ce667b2bf050218796
langan.kg Langan <langan.kg@gmail.com>
2021-06-07 11:36:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,15 +3,14 @@ class Anagram
 
 attr_accessor :word
 
-def initialize(word)
-  @word = word
-end
+  def initialize(word)
+    @word = word
+  end
 
-def match(word)
-  array = []
-  array.select do |w|
-    w.split(" ").sort == @word.split.sort
-
-end
-end
+  def match(word)
+    array = []
+    array.select do |w|
+      w.split(" ").sort == @word.split.sort
+      end
+  end
 end
